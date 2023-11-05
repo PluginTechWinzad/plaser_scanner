0e4a70ffca7e0bb8e7b9ad89f627e8f49372e053
Macos <nguyenvantoan.info@gmail.com>
2023-11-05 22:37:27 +0700

    add LICENSE

--- test/laser_scanner_test.dart
+++ test/laser_scanner_test.dart
@@ -1,6 +1,3 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:laser_scanner/laser_scanner.dart';
-import 'package:laser_scanner/laser_scanner_platform_interface.dart';
 
 void main() {
   
